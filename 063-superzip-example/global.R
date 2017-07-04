b0be65a94f3faa5aea0be8a78770ec76117f69bb
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-04 16:57:47 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -4,3 +4,18 @@ allzips <- read.csv("data/gauge_locations.csv")
 allzips$latitude <- jitter(allzips$latitude)
 allzips$longitude <- jitter(allzips$longitude)
 row.names(allzips) <- allzips$site_no
+
+cleantable <- allzips %>%
+  select(
+    City = 0,
+    State = 0,
+    Zipcode = 0,
+    Rank = 0,
+    Score = 0,
+    Superzip = 0,
+    Population = 0,
+    College = 0,
+    Income = 0,
+    Lat = latitude,
+    Long = longitude
+  )
