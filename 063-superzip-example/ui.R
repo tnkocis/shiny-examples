aa0d0b6b7410f0124324ffe26e592d1c121d7456
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-06 20:53:23 -0700

    Update ui.R

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -26,7 +26,7 @@ navbarPage("Availability of high-magnitude streamflow for groundwater banking in
       # Shiny versions prior to 0.11 should use class="modal" instead.
       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
         draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-        width = 330, height = "auto"
+        width = 330, height = "auto",
 
         h2("Site Manager"),
 
