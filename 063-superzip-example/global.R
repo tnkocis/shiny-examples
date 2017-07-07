0c69731d842597a9f3b145fc0839b6ffb6b170fb
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-06 21:58:17 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -4,15 +4,15 @@ allzips <- readRDS("data/superzip.rds")
 ourdata <- read.csv("data/gauge_locations.csv")
 allzips$latitude <- ourdata$latitude
 allzips$longitude <- ourdata$longitude
+allzips$zipcode <- outdata$site_no
 allzips$college <- allzips$college * 100
-allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
+#allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
 row.names(allzips) <- allzips$zipcode
 
 cleantable <- allzips %>%
   select(
     City = city.x,
     State = state.x,
-    Zipcode = zipcode,
     Rank = rank,
     Score = centile,
     Superzip = superzip,
