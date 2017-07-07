a0e888c45b590b29bfbf695d2733afb7273475a8
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-06 17:12:11 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -22,7 +22,7 @@ function(input, output, session) {
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
-      setView(lng = -120.51, lat = 38.06, zoom = 5)
+      setView(lng = -120.51, lat = 38.06, zoom = 7)
   })
 
   # A reactive expression that returns the set of zips that are
