54341308fa0203af68544f4c2239a240c579deac
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-10 10:09:55 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -6,7 +6,7 @@ allzips$latitude <- ourdata$latitude
 allzips$longitude <- ourdata$longitude
 allzips$zipcode <- ourdata$site_no
 allzips$college <- allzips$college * 100
-allzips$zipcode <- as.character(allzups$zipcode)
+allzips$zipcode <- as.character(allzips$zipcode)
 #formatC(allzips$zipcode, width=7, format="d", flag="0")
 # try as.char
 row.names(allzips) <- allzips$zipcode
