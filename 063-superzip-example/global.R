04bfaea1cc2c12d39a86c9ae85c30320ae279d5e
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-10 10:02:05 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -7,6 +7,7 @@ allzips$longitude <- ourdata$longitude
 allzips$zipcode <- ourdata$site_no
 allzips$college <- allzips$college * 100
 allzips$zipcode <- formatC(allzips$zipcode, width=7, format="d", flag="0")
+# try as.char
 #row.names(allzips) <- allzips$zipcode
 
 cleantable <- allzips %>%
